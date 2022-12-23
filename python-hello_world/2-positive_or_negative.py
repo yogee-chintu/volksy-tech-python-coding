c948ad7e483256b5c6f4b218cfe0a5d17918c95b
root <yogee-chintu>
2022-12-23 12:30:01 +0000

    Task-2

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 if number > 0:
     print("{} is Positive" .format(number))
-elif num == 0:
+elif number == 0:
     print("{} is zero" .format(number))
 else:
     print("{} is negative" .format(number))
