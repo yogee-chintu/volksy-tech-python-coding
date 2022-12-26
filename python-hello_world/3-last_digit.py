e9614384611eec3b9da7f6b6f9395c6a656b21c5
root <yogee-chintu>
2022-12-26 06:52:52 +0000

    Task-3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,7 +8,7 @@ if last == 0:
     line = ('Last digit of ' + str(number) + ' is ' + str(last) + ' and is 0')
 else:
     if last > 5:
-        line = ('Last digit of ' + str(number) + ' is ' + str(last) + ' and is greater ' + 'than 5')
+        line = ('Last digit of ' + str(number) + ' is ' + str(last) + ' and is greater ' + ' than 5')
     elif last < 5:
-        line = ('Last digit of ' + str(number) + ' is ' + str(last) + ' and is less ' + 'than 6 and not 0')
+        line = ('Last digit of ' + str(number) + ' is ' + str(last) + ' and is less ' + ' than 6 and not 0')
 print(line)`
